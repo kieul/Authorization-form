5774a992c4af500f2e029598d5718bece4f70a9c
Linh Kieu <kieu.thuy.linh1@gmail.com>
2023-03-26 19:21:35 +0200

    add project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// the password is: swordfish
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: "password",
+      authorized: false,
+    };
+    this.authorize = this.authorize.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  authorize(e) {
+    const password = e.target.querySelector('input[type="password"]').value;
+    const auth = password == this.state.password;
+    this.setState({
+      authorized: auth,
+    });
+  }
+
+  render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
+    );
+
+    const contactInfo = (
+      <ul>
+        <li>kieu.thuy.linh1@gmail.com</li>
+        <a href="https://www.linkedin.com/in/linh-kieu/">LinkedIn</a>
+        <li>Let's connect!</li>
+      </ul>
+    );
+
+    return (
+      <div id="authorization">
+        <h1>{this.state.authorized ? "Contact" : "Enter the Password"}</h1>
+        {this.state.authorized ? contactInfo : login}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Contact />, document.getElementById("app"));
